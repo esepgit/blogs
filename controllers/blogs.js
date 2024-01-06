45b9116f0cbd5c36ccde7c90f746cf9b217de2a6
K0ftry <ericksepulveda94@hotmail.com>
2024-01-06 17:40:24 -0300

    4.14 ready: put created

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -33,4 +33,23 @@ blogsRouter.get('/', async (request, response) => {
     }
   })
 
+  blogsRouter.put('/:id', async (request, response, next) => {
+    const body = request.body
+
+    const blog = {
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+    }
+
+    try {
+      const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+      response.json(updatedBlog)
+    } catch(exception) {
+      next(exception)
+    }
+    
+  })
+
   module.exports = blogsRouter
