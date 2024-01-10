63ff0f0881bfda080bf153238567f69ca8b0ff98
K0ftry <ericksepulveda94@hotmail.com>
2024-01-10 09:15:22 -0300

    4.20 token extractor in middleware

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -9,13 +9,13 @@ blogsRouter.get('/', async (request, response) => {
     response.json(blogs)
   })
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
+// const getTokenFrom = request => {
+//   const authorization = request.get('authorization')
+//   if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+//     return authorization.substring(7)
+//   }
+//   return null
+// }
   
 blogsRouter.post('/', async (request, response, next) => { 
   const body = request.body
@@ -24,9 +24,9 @@ blogsRouter.post('/', async (request, response, next) => {
     return response.status(400).end()
   }
 
-  const token = getTokenFrom(request)
-  const decodedToken = jwt.verify(token, process.env.SECRET)
-  if (!token || !decodedToken.id) {
+  //const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!request.token || !decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
   const user = await User.findById(decodedToken.id)
